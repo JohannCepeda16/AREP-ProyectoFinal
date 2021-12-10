8e7f50176acb8613ab96c4c373680797d6729ed4
Johann Cepeda <johann.cepeda@mail.escuelaing.edu.co>
2021-12-09 23:19:58 -0500

    amplify aws settings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAKK8HRqbi_BthVyaadSU8430UeC7czP4w",
+  authDomain: "devops-3ce6e.firebaseapp.com",
+  projectId: "devops-3ce6e",
+  storageBucket: "devops-3ce6e.appspot.com",
+  messagingSenderId: "631632953201",
+  appId: "1:631632953201:web:db5840bb3157b01ed800d0",
+  measurementId: "${config.measurementId}",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
